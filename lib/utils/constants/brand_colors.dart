bb294dc857d9d53ab431d540c8cb07c55639fa75
debugtitan <alts.devs@gmail.com>
2026-01-08 19:50:09 +0100

    brand colors

--- lib/utils/constants/brand_colors.dart
+++ lib/utils/constants/brand_colors.dart
@@ -1,6 +1,10 @@
-
+import 'package:flutter/material.dart';
 
 final class BrandColors {
- BrandColors._();
- 
+  BrandColors._();
+
+  static const Color brand = Color(0xFFA65F1D);
+  static const Color primary = Color(0xFF150C04);
+  static const Color neutral = Color(0xFF6F6760);
+  static const Color surface = Color(0xFF15100A);
 }
