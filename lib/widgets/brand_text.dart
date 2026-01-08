e44bd72601d4707dfba3e747a248399aa7497548
debugtitan <alts.devs@gmail.com>
2026-01-08 19:50:43 +0100

    brand text

---
+++ lib/widgets/brand_text.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:vino/utils/constants/brand_colors.dart';
+
+class BrandText extends StatelessWidget {
+  const BrandText({super.key, this.fontSize = 33});
+
+  final double? fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    final parentStyle = Theme.of(context).textTheme.bodyLarge!.copyWith(
+      fontSize: fontSize,
+      fontWeight: FontWeight.w800,
+      height: 45 / fontSize!,
+     
+    );
+
+    return RichText(
+      text: TextSpan(
+        text: "Vino",
+        children: [
+          TextSpan(
+            text: "Wallet",
+            style: parentStyle.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ],
+        style: parentStyle.copyWith(color: BrandColors.brand),
+      ),
+    );
+  }
+}
