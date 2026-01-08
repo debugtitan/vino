46eaa360f5c94e684a77ec02f545178979a53136
debugtitan <alts.devs@gmail.com>
2026-01-08 23:32:35 +0100

    onboarding page router

---
+++ lib/screens/onboarding.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:vino/widgets/brand_text.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: BrandText(fontSize: 23), centerTitle: true),
+    );
+  }
+}
