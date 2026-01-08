3f6a5458eaa2f95154859de4394894dbd961fb5c
debugtitan <alts.devs@gmail.com>
2026-01-08 19:50:57 +0100

    theme config

---
+++ lib/utils/themes/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:vino/utils/themes/dark_mode.dart';
+
+// theme config
+import 'package:vino/utils/themes/light_theme.dart';
+
+class VinoAppTheme {
+  VinoAppTheme._();
+
+  static final ThemeData lightMode = lightTheme;
+  static final ThemeData darkMode = darkTheme;
+}
