508d3c4181c39f447dacf8fd5b1513ae296d1898
debugtitan <alts.devs@gmail.com>
2026-01-08 23:32:24 +0100

    enable  imeersive sticky ui

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,8 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:vino/screens/onboarding.dart';
 import 'package:vino/widgets/brand_text.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.of(
+        context,
+      ).pushReplacement(MaterialPageRoute(builder: (context) => Onboarding())),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  }
 
   @override
   Widget build(BuildContext context) {
