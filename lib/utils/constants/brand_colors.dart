c1c3fb4ff235082376f3f1d81828b1527fe50e0d
debugtitan <alts.devs@gmail.com>
2026-01-09 15:57:49 +0100

    updatedbrand colors

--- lib/utils/constants/brand_colors.dart
+++ lib/utils/constants/brand_colors.dart
@@ -7,4 +7,5 @@ final class BrandColors {
   static const Color primary = Color(0xFF150C04);
   static const Color neutral = Color(0xFF6F6760);
   static const Color surface = Color(0xFF15100A);
+  static const Color neutraDark = Color(0xFFB0A79F);
 }
