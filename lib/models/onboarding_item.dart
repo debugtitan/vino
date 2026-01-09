c3141e24667d47b3c13756da0db3138af70589ff
debugtitan <alts.devs@gmail.com>
2026-01-09 15:58:20 +0100

    Onboard item schema

---
+++ lib/models/onboarding_item.dart
@@ -0,0 +1,11 @@
+class OnboardingItem {
+  const OnboardingItem({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+
+  final String image;
+  final String title;
+  final String description;
+}
