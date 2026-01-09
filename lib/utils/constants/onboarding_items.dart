65af171b0800109cc3a11027dfd413c0145824a6
debugtitan <alts.devs@gmail.com>
2026-01-09 15:58:37 +0100

    defaault onboardingitems

---
+++ lib/utils/constants/onboarding_items.dart
@@ -0,0 +1,30 @@
+import 'package:vino/models/onboarding_item.dart';
+
+const List<OnboardingItem> onboardingItems = [
+  OnboardingItem(
+    image: "assets/images/mobile-pay.png",
+    title: "Effortless Bill Payments & Mobile Recharges",
+    description:
+        "Take the hassle out of paying bills and topping up your mobile services. With our seamless platform, all your essential payments are just a tap away.",
+  ),
+
+  OnboardingItem(
+    image: "assets/images/bitcoin-p2p.png",
+    title: "Secure Peer-to-Peer Crypto Trading",
+    description:
+        "Trade cryptocurrencies with confidence using our secure P2P platform. Connect directly with other users for efficient, hassle-free transactions.",
+  ),
+
+  OnboardingItem(
+    image: "assets/images/rafiki.png",
+    title: "Convert Crypto to Local Currency in Minutes",
+    description:
+        "Our 'Express' service allows you to quickly and easily sell your crypto for local currency, with funds transferred to your bank account in minutes.",
+  ),
+  OnboardingItem(
+    image: "assets/images/cuate.png",
+    title: "Seamless KYC for Merchant Transactions",
+    description:
+        "Stay compliant and secure. Our integrated KYC process ensures that all transactions with merchants meet regulatory standards.",
+  ),
+];
